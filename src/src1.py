13b89055a97b370df1d9b127b9293669dc048a45
Evan-Chan-NEU <105451823+Evan-Chan-NEU@users.noreply.github.com>
2022-08-15 22:28:41 -0400

    Update src1.py

--- src/src1.py
+++ src/src1.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
 df = pd.read_csv('https://raw.githubusercontent.com/Evan-Chan-NEU/DS5220-Final-Project/main/Project%20Dataset/Stores.csv')
 
-df.head()
+sns.pairplot(df, diag_kind='kde')
